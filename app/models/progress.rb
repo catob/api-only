ad296aa5d201e8c6bfcbb56a82a1dbd51ee1a2e8
catob <cato.benjaminsen@gmail.com>
2018-04-23 21:07:48 +0200

    add progress validation

--- app/models/progress.rb
+++ app/models/progress.rb
@@ -1,3 +1,5 @@
 class Progress < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :progress_percent, :level
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # let!(:user) { FactoryBot.create(:user, name: 'catooo', email: 'cato@email.com') }
-  # let!(:progress) { FactoryBot.create(:progress, :progress_percent: 10, level: 1) }
-
   it { should have_one(:progress) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
-
 end
