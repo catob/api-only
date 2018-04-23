78a584f6941fc17cacacbd041dfd4b948151a551
catob <cato.benjaminsen@gmail.com>
2018-04-23 20:40:59 +0200

    add progress model

---
+++ db/migrate/20180423184051_create_progresses.rb
@@ -0,0 +1,10 @@
+class CreateProgresses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :progresses do |t|
+      t.float :progress_percent
+      t.integer :level
+
+      t.timestamps
+    end
+  end
+end
