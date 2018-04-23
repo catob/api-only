4b9ec2221ba23247f24137ae04796e86c501261b
catob <cato.benjaminsen@gmail.com>
2018-04-23 20:50:45 +0200

    set up ref from user to progress

--- db/migrate/20180423184051_create_progresses.rb
+++ db/migrate/20180423184051_create_progresses.rb
@@ -3,6 +3,7 @@ class CreateProgresses < ActiveRecord::Migration[5.2]
     create_table :progresses do |t|
       t.float :progress_percent
       t.integer :level
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,10 @@ ActiveRecord::Schema.define(version: 2018_04_23_184051) do
   create_table "progresses", force: :cascade do |t|
     t.float "progress_percent"
     t.integer "level"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_progresses_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,4 +31,5 @@ ActiveRecord::Schema.define(version: 2018_04_23_184051) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "progresses", "users"
 end
