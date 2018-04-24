77b1b730ada9e52eec665f123093de0d6c56a3db
catob <cato.benjaminsen@gmail.com>
2018-04-24 20:18:37 +0200

    add show action

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe 'User API', type: :request do
       expect(response.body).to match user.name
     end
   end
+
+  describe 'GET /api/v1/users/:id' do
+    before { get "/api/v1/users/#{user.id}" }
+
+    it 'returns user with correct id' do
+      expect(response.status).to eq 200
+      expect(response.body).to match user.name
+    end
+  end
 end
