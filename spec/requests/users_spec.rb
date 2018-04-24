ee445d564c055201f92a7bc2492d562c40b59e8e
catob <cato.benjaminsen@gmail.com>
2018-04-24 20:06:56 +0200

    fix spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe 'Users API', type: :request do
+RSpec.describe 'User API', type: :request do
   let!(:user) { FactoryBot.create(:user) }
 
-  describe 'GET /users' do
-    before { get '/users' }
+  describe 'GET /api/v1/users' do
+    before { get '/api/v1/users' }
 
     it 'returns users' do
       expect(response.status).to eq 200
